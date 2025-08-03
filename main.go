a71f729832cd6f07467783141bed5028d4525d5d
Mohammad Saeed Zare <44446617+MSaeed1381@users.noreply.github.com>
2025-08-03 21:11:24 +0330

    Update main.go

--- main.go
+++ main.go
@@ -23,12 +23,12 @@ import (
 // 	  for every base functionality ( publish, subscribe etc. )
 
 func main() {
-	config := DefaultConfig()
-	go initProfiler(config) // create a webserver for profiling
-	msgStore := initDataStore(config)
-	if msgStore != nil {
-		defer msgStore.Close()
-	} // only for persist store in database (for in-memory data store is nil) // close the data store
+	// config := DefaultConfig()
+	// go initProfiler(config) // create a webserver for profiling
+	// msgStore := initDataStore(config)
+	// if msgStore != nil {
+	// 	defer msgStore.Close()
+	// } // only for persist store in database (for in-memory data store is nil) // close the data store
 	cacheStore := initCacheMemory(config)                                               // create cache store
 	topicStore := memory.NewTopicInMemory(msgStore)                                     // create new topic store
 	prometheusController := initPrometheus(config)                                      // define metric
