2736f94455cbb60337b241ce36aee932c161c8c1
Mohammad Saeed Zare <44446617+MSaeed1381@users.noreply.github.com>
2025-08-14 12:44:47 +0330

    Update broker.go

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -10,10 +10,10 @@ type Message struct {
 	// This parameter is optional. If it's not provided,
 	// the Message can't be accessible through Fetch()
 	// id is unique per every subject
-	Id uint64
-	// Body of the message
-	Body string
-	// The time that message can be accessible through Fetch()
+	// Id uint64
+	// // Body of the message
+	// Body string
+	// // The time that message can be accessible through Fetch()
 	// with the proper Message id
 	// 0 when there is no need to keep message ( fire & forget mode )
 	Expiration time.Duration
